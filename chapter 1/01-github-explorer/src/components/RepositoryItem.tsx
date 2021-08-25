ba4d37ba90eb609b79c9765ba23ee13b5b81ba12
Vitor Pereira de Oliveira <vitorpereira@discente.ufg.br>
2021-08-25 18:08:44 -0300

    :sparkles: Finish chapter 1

---
+++ chapter 1/01-github-explorer/src/components/RepositoryItem.tsx
@@ -0,0 +1,18 @@
+interface RepositoryItemProps {
+  repository: {
+    name: string;
+    description: string;
+    html_url: string;
+  }
+}
+
+export function RepositoryItem(props: RepositoryItemProps) {
+  return (
+    <li>
+      <strong>{props.repository.name}</strong>
+      <p>{props.repository.description}</p>
+
+      <a href={props.repository.html_url} target="_blank" rel="noopener noreferrer">Acessar repositório</a>
+    </li>
+  );
+}
