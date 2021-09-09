92bbc920d25adf35219f5eb9a3ea075d6e8ff8b2
Vitor Pereira de Oliveira <vitorpereira@discente.ufg.br>
2021-09-09 11:48:15 -0300

    :wrench: Add typescript and initial styling

---
+++ ignews/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+function Home() {
+  return (
+    <>
+      <Head>
+        <title>ig.news | Início</title>
+      </Head>
+      <h1>Hello <span>Next</span></h1>
+    </>
+  );
+};
+
+export default Home;
