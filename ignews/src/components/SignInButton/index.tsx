4802d4dcea7ceb9d9be7bed561ebc6f2d011cbc1
Vitor Pereira de Oliveira <vitorpereira@discente.ufg.br>
2021-09-14 16:09:22 -0300

    :wrench: Add next-auth configuration

--- ignews/src/components/SignInButton/index.tsx
+++ ignews/src/components/SignInButton/index.tsx
@@ -1,22 +1,31 @@
+import { signIn, signOut, useSession } from 'next-auth/client';
 import { IoLogoGithub, IoClose } from 'react-icons/io5';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  if (isUserLoggedIn) {
+  if (session) {
     return (
-      <button type="button" className={styles.signInButton}>
+      <button 
+        type="button" 
+        className={styles.signInButton} 
+        onClick={() => signOut()}
+      >
         <IoLogoGithub color="#04d361" />
-        Vitor Pereira
+        {session.user.name}
         <IoClose color="#737380" className={styles.closeIcon} />
       </button>
     );
   }
 
   return (
-    <button type="button" className={styles.signInButton}>
+    <button 
+      type="button" 
+      className={styles.signInButton} 
+      onClick={() => signIn('github')}
+    >
       <IoLogoGithub color="#eba417" />
       Sign in with GitHub
     </button>
