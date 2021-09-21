95831a87d597345a4c8136b060e5527051ba91c4
Vitor Pereira de Oliveira <vitorpereira@discente.ufg.br>
2021-09-21 10:32:55 -0300

    :sparkles: Finish app

--- ignews/src/pages/posts/index.tsx
+++ ignews/src/pages/posts/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 import { RichText } from 'prismic-dom';
 import Prismic from '@prismicio/client';
 
@@ -28,11 +29,13 @@ function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map(post => (
-            <a key={post.slug} href="#">
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a> 
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a> 
+            </Link>
           ))}
         </div>
       </main>
